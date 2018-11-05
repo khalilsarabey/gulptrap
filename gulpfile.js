7dc00052aec2f06fbf86fd3a26ff689fe0a5129a
Khalil Sarabey <hello@khalilsarabey.com>
2018-11-05 18:32:15 +0200

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+"use strict";
+
+const gulp = require('gulp'),
+	  concat = require('gulp-concat'),
+	  uglify = require('gulp-uglify'),
+	  rename = require('gulp-rename'),
+	  sass = require('gulp-sass'),
+	  maps = require('gulp-sourcemaps'),
+	  del = require('del'),
+	  autoprefixer = require('gulp-autoprefixer'),
+	  browserSync = require('browser-sync').create(),
+	  htmlreplace = require('gulp-html-replace'),
+	  cssmin = require('gulp-cssmin');
+
+gulp.task("concatScripts", () => {
+	return gulp.src([
+		'assets/js/vendor/jquery-3.3.1.slim.min.js',
+		'assets/js/vendor/popper.min.js',
+		'assets/js/vendor/bootstrap.min.js',
+		'assets/js/functions.js'
+	])
+		.pipe(maps.init())
+		.pipe(concat('main.js'))
+		.pipe(maps.write('./'))
+		.pipe(gulp.dest('assets/js'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task("minifyScripts", ["concatScripts"], () => {
+  return gulp.src("assets/js/main.js")
+	  .pipe(uglify())
+	  .pipe(rename('main.min.js'))
+	  .pipe(gulp.dest('dist/assets/js'));
+});
+
+gulp.task('compileSass', () => {
+  return gulp.src("assets/css/main.scss")
+    .pipe(maps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("minifyCss", ["compileSass"], () => {
+  return gulp.src("assets/css/main.css")
+    .pipe(cssmin())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('dist/assets/css'));
+});
+
+gulp.task('watchFiles', () => {
+  gulp.watch('assets/css/**/*.scss', ['compileSass']);
+  gulp.watch('assets/js/*.js', ['concatScripts']);
+})
+
+gulp.task('clean', () => {
+  del(['dist', 'assets/css/main.css*', 'assets/js/main*.js*']);
+});
+
+gulp.task('renameFiles', () => {
+  return gulp.src(['*.html', '**/*.php', '!dist', '!dist/**'])
+    .pipe(htmlreplace({
+      'js': 'assets/js/main.min.js',
+      'css': 'assets/css/main.min.css'
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task("build", ['minifyScripts', 'minifyCss'], () => {
+  return gulp.src([
+		'*.html',
+		'favicon.ico',
+		"assets/img/**"
+	], { base: './'})
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('start', ['watchFiles'], () => {
+  browserSync.init({
+  	server: "./"
+  });
+
+  gulp.watch("assets/css/**/*.scss", ['watchFiles']);
+  gulp.watch(['*.html']).on('change', browserSync.reload);
+});
+
+gulp.task("default", ["clean", 'build'], () => {
+  gulp.start('renameFiles');
+});
